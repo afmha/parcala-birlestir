055add575f59311c6f4e669df545c10df4e6e7ab
Afmha <afmha@hotmail.com>
2015-07-27 13:00:17 +0300

    parcala(); fonksiyonuna sınır değişkeni eklendi
    parcala(); fonksiyonuna sınır değişkeni eklendi.Sınır değeri girilmez ise otomatik olarak 255 kabul edilir.

--- parcala-birlestir.php
+++ parcala-birlestir.php
@@ -1,9 +1,9 @@
 <?php
 	
 	class afmha{
-		function parcala($a,$b){
+		function parcala($a,$b,$c){
 			global $parcalanan;
-			$parcalanan = explode($a,$b);
+			$parcalanan = explode($a,$b,$c="255");
 		}
 		function birlestir($a,$b=","){
 			global $birlestirilen;
