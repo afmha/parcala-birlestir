7becd61cc95be41cf8d05dc6c8941d50d58d215e
Afmha <afmha@hotmail.com>
2015-08-06 20:14:49 +0300

    Minimum sistem eklendi
    Minimum yazılım eklendi.

---
+++ parcala-birlestir-min.php
@@ -0,0 +1,1 @@
+<?php class afmha{function parcala($a,$b,$c){global $parcalanan;$parcalanan=explode($a,$b,$c="255");}function birlestir($a,$b=","){global $birlestirilen;$birlestirilen=implode($a,$b);}}$afmha=new afmha();?>
